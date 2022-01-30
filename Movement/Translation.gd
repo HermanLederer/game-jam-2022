2578c76e81deb0bd0ee7eff8d40f3638091e6730
Evgen Troshyn <4901237+trow-eu@users.noreply.github.com>
2022-01-30 15:21:05 +0100

    stuff

--- Movement/Translation.gd
+++ Movement/Translation.gd
@@ -6,7 +6,7 @@ export var hasSoul = true
 onready var col = $Area
 
 func _ready():
-	# visible = false;
+	visible = false;
 
 	col.set_collision_layer_bit(2, hasBody)
 	col.set_collision_layer_bit(6, hasSoul)
