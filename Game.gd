dd91c4be28e219598359c71ff8a44bc0722a6749
Herman Lederer <germans.lederers@gmail.com>
2022-01-30 15:31:16 +0100

    Implement mediation blocks

--- Game.gd
+++ Game.gd
@@ -6,9 +6,16 @@ var _isSoul = false;
 func _ready():
 	add_to_group("duality")
 
+var nextSwitch: int = 0;
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_pressed("ui_accept")):
-		_isSoul = !_isSoul;
-		get_tree().call_group("duality", "toggle")
+	if (OS.get_unix_time() > nextSwitch):
+		if (Input.is_action_just_pressed("ui_accept")):
+			var m = get_node("/root/Game/Level/Meditation").transform.origin;
+			var p = get_node("/root/Game/Player").transform.origin;
+			if (p.distance_to(m) < 1.1 || _isSoul):
+				nextSwitch = OS.get_unix_time() + 1
+				_isSoul = !_isSoul;
+				get_tree().call_group("duality", "toggle")
 
